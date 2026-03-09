98e44f6efbdc6b4e92d92c296bb15e59c6d5004b
Emilson de Souza Nascimento <emilsonsn2@gmail.com>
2026-03-09 01:42:24 -0300

    fix: comment out RepositoryAnalysisSeeder call in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call([RepositoryAnalysisSeeder::class]);
+        // $this->call([RepositoryAnalysisSeeder::class]);
     }
 }
